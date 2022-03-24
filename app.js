140a32ba302a2539d7aa1a3fee51085bb931da2a
chon9 <alexchon9@gmail.com>
2022-03-24 16:31:58 +0800

    load books list

--- app.js
+++ app.js
@@ -21,5 +21,15 @@ connectToDb((err) => {
 
 //routes - res = response , req = request
 app.get('/books', (req,res) => {
-    res.json({msg:"welcome to novel heroes"})
+    let books = []
+    db.collection('novels')
+    .find()
+    .sort({author:1})
+    .forEach(book => books.push(book))
+    .then(()=>{
+        res.status(200).json(books)
+    })
+    .catch(()=> {
+        res.status(500).json({error: 'Could not fetch the documents'})
+    })   
 })
