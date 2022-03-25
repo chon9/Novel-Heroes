8741afd0c6534690e41e3be630276547822ad8f9
chon9 <alexchon9@gmail.com>
2022-03-25 17:22:06 +0800

    Able to read single page using aggregate

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 
 
 const express  = require('express')
+const { ObjectId } = require('mongodb')
 const {connectToDb, getDb} = require('./db')
 
 //init app & middleware
 
 const app = express()
+app.use(express.json())
 
 //db connection
 let db
@@ -32,4 +34,69 @@ app.get('/books', (req,res) => {
     .catch(()=> {
         res.status(500).json({error: 'Could not fetch the documents'})
     })   
+})
+
+app.get('/book/:id', (req,res) => {
+    if(ObjectId.isValid(req.params.id)) {
+        db.collection('novels')
+        .findOne({_id:ObjectId(req.params.id)})
+        .then(book=>{
+            res.status(200).json(book)
+        })
+        .catch(()=> {
+            res.status(500).json({error: 'Could not fetch the documents'})
+        })   
+    }
+    else {
+        res.status(500).json({msg:'Invalid Object Id'})
+    }
+})
+
+app.get('/read/:id/:page', (req,res) => {
+    const pipelines = [
+        [{$match:{_id:ObjectId("623c3fe7db65cd2e2b3c3105")}},{$unwind: "$content"},{$match:{"content.page":"1"}},{$project:{"content.content":1,"content.page":1}}]
+    ]
+    if(ObjectId.isValid(req.params.id)) {
+        db.collection('novels')
+        .aggregate( [{$match:{_id:ObjectId("623c3fe7db65cd2e2b3c3105")}},{$unwind: "$content"},{$match:{"content.page":"1"}},{$project:{"content.content":1,"content.page":1}}])
+        .toArray()
+        .then(page=>{
+            res.status(200).json(page)
+        })
+        .catch(()=> {
+            res.status(500).json({error: 'Could not fetch the documents'})
+        })
+    }
+    else {
+        res.status(500).json({msg:'Invalid Object Id'})
+    }
+})
+
+app.post('/newbook', (req,res) => {
+    //issues to solve, the data should be verified before sent to db
+    const book = req.body
+    db.collection('novels')
+    .insertOne(book)
+    .then(result=>{
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
+app.delete('/removebook/:id', (req,res) => {
+    if(ObjectId.isValid(req.params.id)) {
+        db.collection('novels')
+        .findOne({_id:ObjectId(req.params.id)})
+        .then(result=>{
+            res.status(200).json(result)
+        })
+        .catch(()=> {
+            res.status(500).json({error: 'Could not delete the documents'})
+        })   
+    }
+    else {
+        res.status(500).json({msg:'Invalid Object Id'})
+    }
 })
